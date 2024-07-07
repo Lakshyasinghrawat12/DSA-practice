e83bbaf0092a0c90a251ab3a205aa4d88898fdce
Lakshyasinghrawat12 <122603867+Lakshyasinghrawat12@users.noreply.github.com>
2024-07-07 20:35:04 +0530

    Add files via upload

---
+++ question 1- Tow Sum/question_1.py
@@ -0,0 +1,23 @@
+nums= [2,7,11,15]
+
+seen={} #keep track of numbers (as key) and indexes (as value)
+
+
+target= int(input('enter the target vlaue: ')) #take the target value as input
+
+
+for i, vlaue in enumerate(nums):
+    '''you go over your array using enumerate that 
+    gives you both index and value of elements in array'''
+
+    remaning= target - nums[i]  #store the remaning value 
+
+    if remaning in seen:  
+
+        '''cheks weather the remaning value is already in the directory or not if yes then it return 
+        the indexes of the value and if not it adds it to the directory so that it can be seen in the future 
+        assuming there is a possible solution to the problem''' 
+
+        print( [i, seen[remaning]])
+    else:
+        seen[vlaue]= i
